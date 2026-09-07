890babafa87c2398babbbd3772e2d84ea94ebc7e
Afterdarktale-42 <afterdark.tale@gmail.com>
2026-09-07 08:27:39 +0700

    fix: update seeded email domain to smpn14-surabaya.sch.id

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,21 +13,21 @@ class UserSeeder extends Seeder
         $users = [
             [
                 'name' => 'Administrator SMPN 14',
-                'email' => 'admin@smpn14surabaya.sch.id',
+                'email' => 'admin@smpn14-surabaya.sch.id',
                 'password' => Hash::make('password'),
                 'role' => 'super_admin',
                 'email_verified_at' => now(),
             ],
             [
                 'name' => 'Operator TU Konten',
-                'email' => 'operator@smpn14surabaya.sch.id',
+                'email' => 'operator@smpn14-surabaya.sch.id',
                 'password' => Hash::make('password'),
                 'role' => 'operator',
                 'email_verified_at' => now(),
             ],
             [
                 'name' => 'Kepala Sekolah (Reviewer)',
-                'email' => 'kepsek@smpn14surabaya.sch.id',
+                'email' => 'kepsek@smpn14-surabaya.sch.id',
                 'password' => Hash::make('password'),
                 'role' => 'reviewer',
                 'email_verified_at' => now(),
